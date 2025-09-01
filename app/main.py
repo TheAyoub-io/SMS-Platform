29ebe695d04bf992a98b1c53c06f5122cb92f650
Ahmed-KM-sudo <kassmimohamed93@gmail.com>
2025-09-01 06:12:22 +0100

    fix: move FastAPI app creation above middleware setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import auth, campaigns, contacts, templates, messages, reports, users
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(users.router, prefix="/users", tags=["users"])
